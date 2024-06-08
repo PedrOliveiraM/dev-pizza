56f66f3fa6b8e8321a07fd9324edc12b68b946e3
PedrOliveiraM <pedropucmont@gmail.com>
2024-06-08 11:43:55 -0300

    feat: create first interface and menu

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './**/*.{html,js}', // Ajuste conforme necessário
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        home: "url('/assets/background.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
